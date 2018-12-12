e49dac916919b070ac87599c948bf13174dc6121
nalan ulusoy <nalan.ulusoyy@gmail.com>
2018-12-12 15:54:10 +0300

    tableview example

---
+++ TableViewExample/TableViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewExample
+//
+//  Created by Nalan Ulusoy on 12.12.2018.
+//  Copyright © 2018 Nalan Ulusoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
